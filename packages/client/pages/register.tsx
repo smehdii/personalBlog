4c0740e867819cad0e42dd26c4df7d0e6d987781
Smehdii <siddik.mehdi@gmail.com>
2018-11-22 00:32:52 +0100

    add apolloClient

--- packages/client/pages/register.tsx
+++ packages/client/pages/register.tsx
@@ -1,12 +1,6 @@
-import React, { Component } from "react";
 import Layout from "../components/Layout";
-
-export default class register extends Component {
-  render() {
-    return (
-      <Layout title="Register | Personal Blog">
-        <h1>Register</h1>
-      </Layout>
-    );
-  }
-}
+export default () => (
+  <Layout title="Register | Personal Blog">
+    <div>register</div>
+  </Layout>
+);
